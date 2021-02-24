ac276be4d17dbb983bf995f934c5968a995ceb2c
ikmalshahzan <ikmalshahzann@gmail.com>
2021-02-24 12:22:32 +0800

    first commit
    Got some user login with email verification, registration included

---
+++ header.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+  <!-- Navbar content -->
+  <div class="container">
+        <a class="navbar-brand" href="#">Car Service Booking System</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
+            aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor02">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php"><h6>Home</h6></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php"><h6>About Us</h6></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php"><h6>Locate Us</h6></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="signup.php"><h6>Our Service</h6></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
